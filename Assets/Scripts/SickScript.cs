1b8834f2029081ebcb2d3eb31fbc6732f9010bcd
UniCoti <kimjjhh3103@gmail.com>
2022-09-03 02:57:34 +0900

    Almost Done
    Except Gameover Logic

--- Assets/Scripts/SickScript.cs
+++ Assets/Scripts/SickScript.cs
@@ -9,11 +9,16 @@ public class SickScript : MonoBehaviour
     Rigidbody2D rigid; //스틱의 물리 컴포넌트
     RaycastHit2D hit; //레이를 쓰기 위한 변수
     public Transform Pos; //레이를 쏘기위한 위치, 각도를 위한 오브젝트
+    public GameObject StickPrefab; //막대기 프리팹
+    public GameObject LandPrefab; //지형 프리팹
+
+    GameObject Lands;
 
     // Start is called before the first frame update
     void Start()
     {
        rigid = GetComponent<Rigidbody2D>(); //rigid를 이 스크립트가 있는 오브젝트에게서 가져온다.
+       Lands = GameObject.Find("Lands");
     }
 
     // Update is called once per frame
@@ -42,10 +47,18 @@ public class SickScript : MonoBehaviour
     public void Ray(string Tag , Transform Posi) {
         if(rigid.velocity == Vector2.zero) { //물리.가속도값이 0이라면 -> 움직이지 않는 경우
             hit = Physics2D.Raycast(Posi.position + new Vector3(0.1f , 0,0) , Posi.right, 10); //레이를 Pos의 오른쪽 방향으로 쏨
-            if(hit.collider.tag == Tag) { //만약 레이가 닿은 오브젝트의 태그가 Tag라면 , 성공했을때 다음발판으로 넘어가는 처리.
+            if(hit.collider.CompareTag(Tag)) { //만약 레이가 닿은 오브젝트의 태그가 Tag라면 , 성공했을때 다음발판으로 넘어가는 처리.
                 Player.transform.position = new Vector3(hit.collider.gameObject.transform.position.x ,0,0); //플레이어의 위치를 레이에 맞은 오브젝트의 x값 위로 옮김.
-            } else { //실패했을때. 게임오버 처리
-
+                GameObject Stick = Instantiate(StickPrefab , transform.position, Quaternion.identity); //새 스틱 생성
+                Stick.transform.localScale = new Vector3(0.2f,2,1);
+                Stick.GetComponent<SickScript>().Player = Player; //스틱 프리팹의 플레이어를 현재의 플레이어로 설정
+                Stick.transform.position = Player.transform.position + new Vector3(1,0.1f,0); //스틱 위치를 플레이어 앞으로 이동
+                GameObject Land = Instantiate(LandPrefab);
+                Land.transform.parent = Lands.transform;
+                Land.transform.localScale = new Vector3(Random.Range(1,4), 4,1);
+                Land.transform.position = new Vector3(Player.transform.position.x, -3,0) + new Vector3(Random.Range(5,8), 0,0);               
+                Destroy(this.gameObject); //전 다리를 건넌 막대기 삭제
+                this.enabled = false; //다음 스틱을 만들었을때, 두 스틱 모두 동작하는걸 막기 위한 스크립트 비활성화처리.
             }
         }
     }
